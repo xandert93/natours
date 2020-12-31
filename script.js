522b6f7a52bc1f5036c6c0a6615ae3d11845b37e
Xander T <xandert.93@outlook.com>
2020-12-31 01:02:54 +0000

    add intersection observer transitionend cleanup

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 const faders = document.querySelectorAll('.fade-in'),
   sliders = document.querySelectorAll('.slide-in');
 
-function intersectionHandler(entriesArr) {
-  entriesArr.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      return;
-    } else {
-      entry.target.classList.add('appear');
-      aosObserver.unobserve(entry.target);
-    }
-  });
-}
-
 const aosObserver = new IntersectionObserver(intersectionHandler, {
   threshold: 0,
   rootMargin: '0px 0px -300px 0px',
@@ -19,3 +8,22 @@ const aosObserver = new IntersectionObserver(intersectionHandler, {
 
 faders.forEach((fader) => aosObserver.observe(fader));
 sliders.forEach((slider) => aosObserver.observe(slider));
+
+function intersectionHandler(entriesArr) {
+  entriesArr.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      aosObserver.unobserve(entry.target);
+      entry.target.ontransitionend = () =>
+        entry.target.classList.remove(
+          'fade-in',
+          'slide-in',
+          'appear',
+          'left',
+          'right'
+        );
+    }
+  });
+}
